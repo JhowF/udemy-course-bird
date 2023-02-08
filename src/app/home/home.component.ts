1a5c1ee3a36c5ab99b9b70869ddfeba4814fb09f
jhowf <jonathanferreirar@gmail.com>
2023-02-08 17:55:20 -0300

    adding several funcitons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['./home.component.css'],
+  providers: [OfertasService],
 })
 export class AppHomeComponent implements OnInit {
-  fisrt = new OfertasService();
+  public oferta!: Oferta[];
 
-  jonh: any = [];
-
-  constructor() {}
+  constructor(private ofertaservice: OfertasService) {}
 
   ngOnInit(): void {
-    /* this.fisrt.getOfertas().map((number) => this.jonh.push(number));
+    //  this.oferta = this.ofertaservice.getOfertas();
+
+    this.ofertaservice
+      .getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        console.log('function resolved after 3 seconds');
+        this.oferta = ofertas;
+      })
+      .catch((erro) => console.log(erro));
+
+    /* 
+    
+    fisrt = new OfertasService();
+
+    jonh: any = [];
+    
+    this.fisrt.getOfertas().map((number) => this.jonh.push(number));
     console.log(`Este e o Arrey Jonathan`, this.jonh);       MANEIRA ERRADA DE EXTRAIR SERVICO USANDO O ANGULAR*/
   }
 }
